27542586f0b0dc6b55446142713383a5047e853e
Diego Bravo <diegobravo8702@gmail.com>
2021-09-23 08:18:39 -0500

    algoritmo usando expresiones regulares horizontal, vertica y diagonales

--- src/main/java/com/bravo/meli/adn/negocio/algoritmos/AlgoritmoRegex.java
+++ src/main/java/com/bravo/meli/adn/negocio/algoritmos/AlgoritmoRegex.java
@@ -5,27 +5,120 @@ import java.util.regex.Matcher;
 import com.bravo.meli.adn.negocio.Analizable;
 import com.bravo.meli.adn.negocio.excepciones.DatosInvalidosException;
 
+/**
+ * 
+ * @author diego
+ *
+ */
 public class AlgoritmoRegex extends Algoritmo {
 
+	/**
+	 * 
+	 * @param cadenas
+	 * @return
+	 */
 	@Override
 	public boolean isMutant(String[] cadenas) throws DatosInvalidosException {
-		validarCadenas(cadenas);
+		// validarCadenas(cadenas);
 
 		Matcher matcher;
-		for (String cadena : cadenas) {
-			matcher = patronMutante.matcher(cadena);
-			if (matcher.find()) {
-				return true;
+
+		matcher = patronMutante.matcher(condensarCadenas(cadenas));
+		if (matcher.find()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Transforma el array de cadenas de adn en un string que inckluye todos los
+	 * casos de evaluaciuon: cadenas horizontales, verticales y diagonales
+	 * 
+	 * @param cadenas
+	 * @return cadenaCondensada
+	 */
+	private String condensarCadenas(String[] cadenas) {
+		final int indice = cadenas.length;
+		final String separador = "_";
+
+		StringBuilder cadenasCombinadas = new StringBuilder();
+		StringBuilder cadenaHorizontales = new StringBuilder();
+		StringBuilder cadenaVerticales = new StringBuilder();
+		StringBuilder cadenaDiagonalA = new StringBuilder();
+		StringBuilder cadenaDiagonalB = new StringBuilder();
+
+		// con dos indices se recorre la totalidad de la matriz
+		for (int fila = 0; fila < indice; fila++) {
+			cadenaHorizontales.append(separador);
+			cadenaVerticales.append(separador);
+			cadenaDiagonalA.append(separador);
+			cadenaDiagonalB.append(separador);
+
+			// se llena la nueva cadena con horizontales. No requiere doble indice.
+			cadenaHorizontales.append(cadenas[fila]);
+
+			// Esta se utiliza para indicar si en el recorrido por las diagolanes nos
+			// encontramos evaluando la diagonal antes de salirnos del arreglo. Cuando esto
+			// ocurre se recalculan los indices para continuar leyendo la matriz al otro
+			// lado de donde se excedió el limite.
+			boolean parteSuperior = true;
+			for (int columna = 0; columna < indice; columna++) {
+				// Se llena la cadena de verticales: se invierte filas por columnas
+				cadenaVerticales.append(cadenas[columna].charAt(fila));
+
+				// Esto evalua si nos "salimos" por arriba de la matriz, de ser así modificamos
+				// la bandera parteSuperior para indicar que a partir de ahora se utilizará los
+				// indices de la matriz pero recalculados. Para continuar "por abajo" de la
+				// matriz
+				if (fila - columna >= 0) {
+					// Llenando en la diagonal principal: inf izq -- sup der PARTE SUPERIOR
+					cadenaDiagonalA.append(cadenas[fila - columna].charAt(columna));
+					// Llenando en la diagonal inversa: sup izq -- inf der PARTE SUPERIOR
+					cadenaDiagonalB.append(cadenas[(indice - 1) - Math.abs(fila - columna)].charAt(columna));
+				} else {
+					if (parteSuperior) {
+						// Como se debe continuar con la evaluacion en la parte inferior se añade un
+						// espacio para separar la cadena
+						cadenaDiagonalA.append(separador);
+						cadenaDiagonalB.append(separador);
+						parteSuperior = false;
+					}
+					// Llenando en la diagonal principal: inf izq -- sup der PARTE INFERIOR
+					cadenaDiagonalA.append(cadenas[indice - Math.abs(fila - columna)].charAt(columna));
+					// Llenando en la diagonal inversa: sup izq -- inf der PARTE INFERIOR
+					cadenaDiagonalB.append(cadenas[Math.abs(fila - columna)].charAt(columna));
+				}
 			}
 		}
-		return false;
+
+		cadenasCombinadas.append(cadenaHorizontales.toString());
+		cadenasCombinadas.append(cadenaVerticales.toString());
+		cadenasCombinadas.append(cadenaDiagonalA.toString());
+		cadenasCombinadas.append(cadenaDiagonalB.toString());
+
+		System.out.println();
+		System.out.println(cadenasCombinadas.toString());
+
+		return cadenasCombinadas.toString();
 	}
 
 	public static void main(String args[]) {
 		// String cadenas[] = null;// = { "aaat", "", "", "" };
-		String cadenas[] = { "aaat", "aaag", "aaag", "agaa" };
+		//@formatter:off
+		String cadenas[] = { 
+				"acfjouCK",
+				"beintBJR", 
+				"dhmsAIQY", 
+				"glrzHPXd", 
+				"kqyGOVch", 
+				"pxFNUbgk", 
+				"vEMTafjm",
+				"DLSZeiln", };
+		//@formatter:on
 		Analizable analizador = new AlgoritmoRegex();
 		try {
+			System.out.println();
 			System.out.println(analizador.isMutant(cadenas) ? "Mutante" : "Muggle");
 		} catch (DatosInvalidosException e) {
 			System.out.println(e);
